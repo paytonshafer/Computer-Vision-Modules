dddad52e5e49e91b555370bd5c2cc87d0b6b3c6e
paytonshafer <paytonshafer@gmail.com>
2023-09-13 17:58:32 -0400

    added fingers up and find distance functions

--- Hand-Tracking/hand_track_module.py
+++ Hand-Tracking/hand_track_module.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+from math import sqrt
 
 class HandDetector:
     def __init__(self, mode=False, maxHands=2, modelComplexity=1,detectionConf=0.5, trackConf=0.5):
@@ -46,3 +47,31 @@ class HandDetector:
 
         #return the list of landmarks
         return lmList
+
+    #function to test if fingers are up, returns a list of len 5 where 1 = up and 0 = down
+    def fingersUp(self):
+        fingers = []
+        # Thumb
+        if len(self.lmList) != 0:
+            if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+            # Fingers
+            for id in range (1, 5):
+                if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+
+        return fingers
+    
+    #function to find the distance between two landmarks where p1 and p2 are landmark ids
+    def findDistance(self, p1, p2, img):
+        #find the length
+        length = sqrt((self.lmList[p1][1] - self.lmList[p2][1])**2 + (self.lmList[p1][2] - self.lmList[p2][2])**2)
+
+        #draw a connector line on the hand
+        img = cv2.line(img, (self.lmList[p1][1], self.lmList[p1][2]), (self.lmList[p2][1], self.lmList[p2][2]), (0,0,255),2)
+
+        return length, img
